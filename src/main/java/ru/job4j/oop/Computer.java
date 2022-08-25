6d109ffee01ba5983cf0b2daaf6e40128b727e3e
Станислав <qpoto@yandex.ru>
2022-08-25 11:02:16 +0300

    1.7. Конструктор [#173889 #336000]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -2,12 +2,6 @@ package ru.job4j.oop;
 
 public class Computer {
 
-    public void printInfo() {
-        System.out.println("Много мониторов " + multiMonitor);
-        System.out.println("SSD " + ssd + " GB");
-        System.out.println("Модель CPU " + cpu);
-    }
-
     private boolean multiMonitor;
     private int ssd;
     private String cpu;
@@ -16,12 +10,18 @@ public class Computer {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
-         }
+    }
+
+    public void printInfo() {
+        System.out.println("Много мониторов " + multiMonitor);
+        System.out.println("SSD " + ssd + " GB");
+        System.out.println("Модель CPU " + cpu);
+    }
 
     public static void main(String[] args) {
         Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
         computer.printInfo();
         Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
-        computer.printInfo();
+        comp.printInfo();
     }
 }
