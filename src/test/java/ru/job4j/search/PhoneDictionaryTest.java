727d1820cc92885187da03396165511fc19a0bc6
Станислав <qpoto@yandex.ru>
2022-09-18 18:57:48 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #343939]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -15,4 +15,13 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary(); // Создаем новую "записную книжку"
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk")); // Добавляем туда новый контакт
+        ArrayList<Person> persons = phones.find("Efim"); //Записная книжка ищет Ефима
+        assertThat(persons.size()).isEqualTo(0); // Так Ефим стройкой выше не найден, то и коллекция не создается
+        // (не возвращается, а соответственно ее не и размер ее равен 0, поэтому тут проверяем, что размер равен 0.
+    }
 }
