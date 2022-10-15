022e042c8d893047b1582add089c663a80598493
Станислав <qpoto@yandex.ru>
2022-10-15 11:28:52 +0300

    9.2. Тесты вывода на консоль [#33585 #351558]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,7 +1,9 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class StartUITest {
 
