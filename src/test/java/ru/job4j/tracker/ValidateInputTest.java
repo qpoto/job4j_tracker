e949bce2f80d1bddabbb72b03e6a6fdff26c164f
Станислав <qpoto@yandex.ru>
2022-11-02 22:35:03 +0300

    2.1. Тест на ValidateInput [#311887 #356853]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -46,6 +46,6 @@ public class ValidateInputTest {
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(1);
+        assertThat(selected).isEqualTo(-1);
     }
 }
