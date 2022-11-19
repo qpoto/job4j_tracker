6f8b5c6a4649e5658a08cc1e72f6c0fd29d8d0fd
Станислав <qpoto@yandex.ru>
2022-11-19 21:44:17 +0300

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,35 @@
+package ru.job4j.ooa;
+
+public final class Airbus {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        String enginQ = name.equals("A380") ? "4 двигателя" : "Количество двигателей: " + COUNT_ENGINE;
+        System.out.println(enginQ);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
