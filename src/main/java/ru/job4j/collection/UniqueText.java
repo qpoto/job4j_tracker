33f1091c2a24df5baceba4302a8f83fd9831edff
Станислав <qpoto@yandex.ru>
2022-12-06 13:34:05 +0300

    2. Идентичные тексты [#212655 #368104]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String te : text) {
             if (!check.contains(te)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
