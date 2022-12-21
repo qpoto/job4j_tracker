64b3a5d7697dcbbb105dc9ceba99e3faf3cc3523
Станислав <qpoto@yandex.ru>
2022-12-21 08:34:16 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ItemAscByNameTest {
+
+    @Test
+    public void compare() {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(2, "Second"));
+        items.add(new Item(3, "Third"));
+        items.add(new Item(1, "First"));
+        items.sort(new ItemAscByName());
+        System.out.println(items);
+
+        List<Item> expected = new ArrayList<>();
+        items.add(new Item(1, "First"));
+        items.add(new Item(2, "Second"));
+        items.add(new Item(3, "Third"));
+
+    }
+}
