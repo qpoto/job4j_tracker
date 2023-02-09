77543e0b43cfc24a1db38506321f658925f59f2a
Станислав <qpoto@yandex.ru>
2023-02-09 08:40:15 +0300

    2.1. Лямбда блок [#249208 #389887]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class LambdaUsage {
+public class LambdaUsage  {
 
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
