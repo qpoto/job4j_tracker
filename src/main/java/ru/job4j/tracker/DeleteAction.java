e69694780008f1ffff2cdbe1f05df992193f4c3d
Стас <qpoto@yandex.ru>
2024-04-20 14:43:22 +0300

    2. Трекер SQL [#1734] Рефакторинг: 1. Изменен тип возращаемого значения в методе delete() c boolean не void, а так же внесение изменений в другие классы связанные с этим методом 2. Добавлены однострочные комментарии-вопросы ментору в классе SqlTracker

--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -15,7 +15,9 @@ public class DeleteAction implements UserAction {
     public boolean execute(Input input, Store tracker) {
         out.println("=== Delete item ===");
         int id = input.askInt("Enter id: ");
-        if (tracker.delete(id)) {
+        int itemId = tracker.findById(id).getId();
+        if (itemId > 0) {
+            tracker.delete(itemId);
             out.println("Заявка удалена успешно.");
         } else {
             out.println("Ошибка удаления заявки.");
--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -44,31 +45,42 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
+        int id = item.getId();
+        String name = item.getName();
+        //Далее что работа с Insert
         return null;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        return false;
+        String name = item.getName();
+        //Далее найти заявки по id и перезаписать данми
+        return true;
     }
 
     @Override
-    public boolean delete(int id) {
-        return false;
+    public void delete(int id) {
+        //Тут работа с Delete from items where id = id
     }
 
     @Override
     public List<Item> findAll() {
-        return List.of();
+        List<Item> items = new ArrayList<>();
+        //Тут c select * from items, с созданием Item и добавлением в allItems
+        return items;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return List.of();
+        List<Item> items = new ArrayList<>();
+        //Тут c select * from items where name = key, с созданием Item и добавлением в allItems
+        return items;
     }
 
     @Override
     public Item findById(int id) {
-        return null;
+        Item item = new Item();
+        //Select * from users where id = id;
+        return item;
     }
 }
